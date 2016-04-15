c9f2370a3a10b2c6048590bf853becdfb3d78021
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-15 17:20:32 +0800

    第二课printf的语法学习

--- C语言数据类型与运算符/HelloWorld/HelloWorld/main.c
+++ C语言数据类型与运算符/HelloWorld/HelloWorld/main.c
@@ -10,7 +10,11 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    // 打印消息
+    //print语法
+    //1.printf("你想输入的东西");
+    //2.printf("格式字符串",...)
+    //%d整型%f浮点数%s字符串%c单个字符%g去掉小数后面的0
     printf("Hello, World!\n");
+    printf("num = %d\n",12);
     return 0;
 }
