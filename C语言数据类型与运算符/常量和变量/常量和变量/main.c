970e3dfd9eb7dc4d647eb714944b0af07b02a6c5
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-15 17:50:01 +0800

    常量和变量单打印显示

--- C语言数据类型与运算符/常量和变量/常量和变量/main.c
+++ C语言数据类型与运算符/常量和变量/常量和变量/main.c
@@ -10,6 +10,12 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    //打印常量
+    printf("%d,%g,%c,%s\n",-12,1.234,'a',"你好");
+    //打印变量
+    // 字符char 短整型short 整型int 长整型long 单精度浮点型float 双精度浮点型double
+    int a = 10;
+    char b = 'b';
+    printf("%d,%c\n",a,b);
     return 0;
 }
