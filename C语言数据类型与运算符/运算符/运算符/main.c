57674fda0f36b037cfea2477d8ec69fa65a5993c
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-16 07:23:53 +0800

    切记除了a++单独写一行，其他任何时候，都需要看是先加还是后加

--- C语言数据类型与运算符/运算符/运算符/main.c
+++ C语言数据类型与运算符/运算符/运算符/main.c
@@ -14,7 +14,8 @@ int main(int argc, const char * argv[]) {
     int b,c;
     b = a++;
     c = ++a;
-    printf("a=%d,b=%d,c=%d\n",a,b,c);
+    printf("a=%d,b=%d,c=%d\n",a,b++,c);
     //a=3,b=1,c=3这是运算的结果原因如下＋＋在后的时候赋值，则先使用a的数值，使用后在＋1所以运行来b＝a++后，b＝1，a＝2，再运行c那条的时候，c就是a先加1再赋值了
+    //切记除了a++单独写一行，其他任何时候，都需要看是先加还是后加
     return 0;
 }
