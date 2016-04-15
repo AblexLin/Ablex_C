ebc8b60a4acc1a84c960308653d0560b4775102c
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-15 19:19:27 +0800

    添加一个加法的计算方式，让你输入第一个和第二个数据，然来你再去相加输出

--- C语言数据类型与运算符/scanf/scanf/main.c
+++ C语言数据类型与运算符/scanf/scanf/main.c
@@ -9,7 +9,13 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    //scanf语法
+    //scanf("数据类型的占位符",地址)
+    int a,b;
+    printf("请输入第一个数:");
+    scanf("%d",&a);
+    printf("请输入第二个数:");
+    scanf("%d",&b);
+    printf("%d+%d=%d\n",a,b,a+b);
     return 0;
 }
