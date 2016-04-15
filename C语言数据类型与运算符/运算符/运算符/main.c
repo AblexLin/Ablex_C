47585c1c26e53cb216e48199bb2a92693f8d2865
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-16 07:21:42 +0800

    添加＋＋的前后使用方式，有什么区别

--- C语言数据类型与运算符/运算符/运算符/main.c
+++ C语言数据类型与运算符/运算符/运算符/main.c
@@ -10,6 +10,11 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    int a=1;
+    int b,c;
+    b = a++;
+    c = ++a;
+    printf("a=%d,b=%d,c=%d\n",a,b,c);
+    //a=3,b=1,c=3这是运算的结果原因如下＋＋在后的时候赋值，则先使用a的数值，使用后在＋1所以运行来b＝a++后，b＝1，a＝2，再运行c那条的时候，c就是a先加1再赋值了
     return 0;
 }
