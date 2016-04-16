931799efa374f88c5b9e41e37dbeb680011a4eff
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-16 10:04:43 +0800

    使用if elseif来实现0～9数字转换为零～玖

--- C语言数据类型与运算符/switch语句学/switch语句学/main.c
+++ C语言数据类型与运算符/switch语句学/switch语句学/main.c
@@ -9,7 +9,31 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int a;
+    printf("请输入0～9的数字:");
+    scanf("%d",&a);
+    if (a==0) {
+        printf("零\n");
+    } else if(a==1){
+        printf("壹\n");
+    } else if(a==2){
+        printf("贰\n");
+    } else if(a==3){
+        printf("叁\n");
+    } else if(a==4){
+        printf("肆\n");
+    } else if(a==5){
+        printf("伍\n");
+    } else if(a==6){
+        printf("陆\n");
+    } else if(a==7){
+        printf("柒\n");
+    } else if(a==8){
+        printf("捌\n");
+    } else if(a==9){
+        printf("玖\n");
+    } else {
+        printf("无法转换\n");
+    }
     return 0;
 }
