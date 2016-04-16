7c0a9f7cae08f7a149162e6e5582f9df9c722312
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-16 09:01:01 +0800

    goto基本不用，添加简单的if语句使用，例子输入一个偶数，如果是偶数那么打印出来

--- C语言数据类型与运算符/goto语句/goto语句/main.c
+++ C语言数据类型与运算符/goto语句/goto语句/main.c
@@ -9,7 +9,14 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+//    cong  shang wang xia zhixing
+    int a;
+    printf("请输入一个数字:");
+    scanf("%d",&a);
+    if (a%2 == 0) {
+        printf("您输入的%d是一个偶数!\n",a);
+    }
+//    alt+command+左右按键可以收起或者打开一个{}/*。。*/等等
+    //条件可以放常量，变量，运算式，比较式
     return 0;
 }
