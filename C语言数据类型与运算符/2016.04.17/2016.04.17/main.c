4e987e828cf799082730d54816bbcd8cc3834f38
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-18 10:58:57 +0800

    给数据加上注释病运行ok，没出错

--- C语言数据类型与运算符/2016.04.17/2016.04.17/main.c
+++ C语言数据类型与运算符/2016.04.17/2016.04.17/main.c
@@ -9,31 +9,32 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-//    int i,j;
-//    for (i=3; i<4; i++)
-//    {
-//        printf("循环内i=%d\n",i);
-//        for (j=2; j<5; j++)
-//        {
-//             printf("循环内j=%d\n",j);
-//        }
-//    }
-//    printf("循环外i=%d\n",i);
-//    int a,b,c;
-//    for (a=0;a<10;a++){
-//        for (b=0;b<10;b++){
-//            for (c=0;c<10;c++){
-//                if((a*100+b*10+c)+(c*100+b*10+a)==1333){
-//                    printf("a=%d,b=%d,c=%d\n",a,b,c);
-//                }
-//            }
-//        }
-//    }
-//    return 0;
+    printf("1.计算内外循环的嵌套\n");
     int i,j;
-    for (i=1; i<6; i++) {
-        j+=i*i;
+    for (i=3; i<4; i++)
+    {
+        printf("循环内i=%d\n",i);
+        for (j=2; j<5; j++)
+        {
+             printf("循环内j=%d\n",j);
+        }
     }
-    printf("1～5的阶乘等于%d\n",j);
+    printf("循环外i=%d\n",i);
+    printf("2.计算abc＋cba＝1333的数有几组\n");
+    int a,b,c;
+    for (a=0;a<10;a++){
+        for (b=0;b<10;b++){
+            for (c=0;c<10;c++){
+                if((a*100+b*10+c)+(c*100+b*10+a)==1333){
+                    printf("a=%d,b=%d,c=%d\n",a,b,c);
+                }
+            }
+        }
+    }
+    printf("3.计算1～5的阶乘总和\n");
+    int x,y;
+    for (x=1; x<6; x++) {
+        y+=x*x;
+    }
+    printf("1～5的阶乘等于%d\n",y);
 }
