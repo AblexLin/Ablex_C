65407dcd2c1c198590d872917d0a0a5ffabb02b0
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-18 13:03:57 +0800

    使用for循环while和do while循环实现水仙花数

--- C语言数据类型与运算符/do while/do while/main.c
+++ C语言数据类型与运算符/do while/do while/main.c
@@ -9,7 +9,38 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    //for 循环实现水仙花数
+    int i,j,k,n=100;
+    /*printf("'water flower'number is:");
+    for(n=100;n<1000;n++)
+    {
+        i=n/100;//分解出百位
+        j=n/10%10;//分解出十位
+        k=n%10;//分解出个位
+        if(n==i*i*i+j*j*j+k*k*k)
+        {
+            printf("%-5d",n);
+        }
+    }*/
+    //while 循环实现水仙花数
+    /*while (n<1000) {
+        i=n/100;
+        j=n/10%10;
+        k=n%100%10;
+        if (n==i*i*i+j*j*j+k*k*k) {
+            printf("这是水仙花数:%d\n",n);
+        }
+        n++;
+    }*/
+    //do while 循环实现水仙花数
+    do{
+        i=n/100;
+        j=n/10%10;
+        k=n%100%10;
+        if (n==i*i*i+j*j*j+k*k*k) {
+            printf("这是水仙花数:%d\n",n);
+        }
+        n++;
+    }while(n<1000);
     return 0;
 }
