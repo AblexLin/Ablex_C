2ec5685efbfc196b4d76b82cae8ddf48c56e234c
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-20 17:03:13 +0800

    添加一维数组，二维数组，指针的混合利用 1.添加一维数组和函数的混合使用 2.二维数组不能使用指针变量传递数据 3.sizeof的使用 4.字符串和数组一样，它们的名字都是地址

--- C语言之指针/数组和指针的关系/数组和指针的关系/main.c
+++ C语言之指针/数组和指针的关系/数组和指针的关系/main.c
@@ -7,9 +7,36 @@
 //
 
 #include <stdio.h>
-
+void acc(int *p);
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    //一维数组的
+    /*int a[10];
+    for (int i=0; i<10; i++) {
+        a[i]=i+1;
+    }
+    int * p=a;
+    printf("一维数组a[2]=%d,p[2]=%d\n",*a+2,*(p+2));
+    
+    for (int i=0; i<10; i++) {
+        printf("a[%d]=%d\n",i,*(a+i));//数组的遍历
+    }
+    acc(a);//这其实是把首地址传递进来，数组的名字就是首地址*/
+    //数据类型sizeof(type or 表达式expression)
+    //printf("%d\n",sizeof(double));
+    /*int a[3]={0};
+    for (int i=0; i<sizeof(a)/sizeof(int); i++) {
+        printf("%d\n",a[i]);//sizeof(a)/sizeof(int)能算出数组有多少个变量
+    }*/
+    //二维数组不能用指针，直接传递a[][]
+    //字符串
+    char ch[]="hello";
+    char *p="hellow";//这说明字符串也是地址
+    char *name="顾明轩";//中文也属于字符串，不能使用字符变量来定义
+    char name2[]="顾明轩";
+    printf("%s,%s,%s,%s\n",ch,p,name,name2);
     return 0;
+}
+void acc(int *p){
+    printf("一维数组%d\n",*(p+2));
+    printf("一维数组%d\n",p[10]);
 }
