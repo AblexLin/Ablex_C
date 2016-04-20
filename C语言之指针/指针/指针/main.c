462a7c741926e1da0fec12e1500e731d7b89a3e1
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-20 15:46:09 +0800

    添加指针的两种使用方式1.读取其他函数内部数据的时候2.动态申请内存。好处在于可以使用变量来申请数组

--- C语言之指针/指针/指针/main.c
+++ C语言之指针/指针/指针/main.c
@@ -7,19 +7,43 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a,int *b);
 
 int main(int argc, const char * argv[]) {
-//    int a ;
+    //指针变量介绍
+    /*//    int a ;
 //    a=199;
 //    printf("%p\n",&a);
 //    printf("%d\n",*&a);
-    //&变量--取地址
-    //*地址--取值
-    //指针变量
-    int a;
-    int *p;//指针变量
-    p=&a;//指针变量初始化
-    *p=100;//其实就是a＝100
-    printf("%d",a);
+//    //&变量--取地址
+//    //*地址--取值
+//    //指针变量
+//    int a;
+//    int *p;//指针变量
+//    p=&a;//指针变量初始化
+//    *p=100;//其实就是a＝100
+//    printf("%d",a);*/
+    //指针实例
+    //1.在一个函数里想访问另外一个函数里的变量
+    int a=1;
+    int b=2;
+    printf("a=%d,b=%d\n",a,b);
+    swap(&a,&b);
+    printf("a=%d,b=%d\n",a,b);
+    //2.动态的内存申请(在堆里面申请)
+    int student_count=5;
+    int * students=malloc(sizeof(int)*student_count);
+    students[3]=121;
+    printf("%d\n",students[3]);
+    free(students);//切记释放内存
     return 0;
 }
+void swap(int *a,int *b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+
