1c625e9da4fba18b653e6fb40b80013adfd19f8f
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-21 17:48:04 +0800

    文件操作

---
+++ 文件操作/文件操作/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  文件操作
+//
+//  Created by 顾明轩 on 16/4/21.
+//  Copyright © 2016年 顾明轩. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
