4cdd8a4a102eee6542fdf47168d9c91cc9a92f59
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-22 07:33:18 +0800

    添加写入一个字符到文件的方法

--- 文件操作/文件操作/main.c
+++ 文件操作/文件操作/main.c
@@ -7,9 +7,20 @@
 //
 
 #include <stdio.h>
+//文件操作常用函数
+//fputc 输入一个字符
+//fputs 输入一个字符串
+//fprintf 按照printf的方式输入
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    FILE *student_score_file=NULL;
+    student_score_file=fopen("studentscore.txt", "w");
+
+    if (student_score_file==NULL) {
+        printf("打开文件失败");
+    }
+    
+    fputc('c', student_score_file);
+    fclose(student_score_file);
     return 0;
 }
