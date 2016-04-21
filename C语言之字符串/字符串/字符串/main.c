b94ce192a24f5897da5dfe9ec9e95733595c2a26
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-21 09:52:12 +0800

    添加字符串的使用

--- C语言之字符串/字符串/字符串/main.c
+++ C语言之字符串/字符串/字符串/main.c
@@ -7,9 +7,39 @@
 //
 
 #include <stdio.h>
-
+#include <string.h>
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+//    char name[]="你好";//数组大小是7个
+//    char *pname=name;
+//    printf("%s,%s\n",name,pname);
+//    char name[][20]={"顾明轩","王爱平"};
+//    char *students[2];
+//    students[0]="轩爷";
+//    students[1]="老人头";
+//    printf("%s,%s\n",*students,students[1]);
+//    char *str="12345";
+//    int c=0;
+//    for (int i=0; i<20; i++) {
+//        if (str[i]!='\0') {
+//            c++;
+//        }
+//        else{
+//            break;
+//        }
+//    }
+//    printf("%d\n",c);//输出字符串的长度
+//    
+//    char *sss="轩爷";
+//    printf("%d",strlen(sss));//不包含'\0'的函数
+    char sss[]="顾明轩";
+    if (strcmp("顾明轩", sss)==0) {
+        printf("正确\n");
+    }
+    char cp[10];
+    strcpy(cp, sss);
+    printf("%s\n",cp);
+    char cat[20]="帅哥";
+    strcat(cat, sss);
+    printf("%s\n",cat);
     return 0;
 }
