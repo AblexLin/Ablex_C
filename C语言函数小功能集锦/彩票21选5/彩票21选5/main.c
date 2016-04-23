e108916b3c3de3e0b8db03c8cb68a3cd53166999
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-24 07:31:30 +0800

    添加彩票21选5小游戏

---
+++ C语言函数小功能集锦/彩票21选5/彩票21选5/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  彩票21选5
+//
+//  Created by 顾明轩 on 16/4/24.
+//  Copyright © 2016年 顾明轩. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
