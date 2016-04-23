7360cc1b220c4165b7ab71ba0609f8cbbd2b6985
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-23 17:18:48 +0800

    ***添加二维数组的循环输入～～～保存字符串～啦啦啦～～～

--- C语言之字符串/尝试字符串数组的循环赋值/尝试字符串数组的循环赋值/main.c
+++ C语言之字符串/尝试字符串数组的循环赋值/尝试字符串数组的循环赋值/main.c
@@ -7,9 +7,23 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#define COUNT 3//数组有多大？我不知，但是定义个宏啊，随时修改，66666666666
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+//    char name[3][20]={{"xuan"},{"shou"},{"wang"}};
+    char ha[20];//这个数组干嘛？存键盘输入的数据啊～～当然输入的肯定是字符串啦～～～
+    
+    char name[COUNT][20]={};//数组定义的时候必须先初始化，那么就先初始化为空嘛
+    
+    for (int i=0; i<COUNT; i++) {
+        printf("请输入:");
+        scanf("%s",ha);//这是什么？当人是吧键盘输入的字符串给保存到ha这个数组的首地址啦～～～
+        strcpy(name[i], ha);//循环给数组增加内容不就好了么？哈哈哈
+    }
+    for (int i=0; i<3; i++) {
+        printf("%s\n",name[i]);
+    }
+
     return 0;
 }
