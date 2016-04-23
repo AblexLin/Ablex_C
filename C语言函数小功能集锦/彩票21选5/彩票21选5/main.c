078cf69a15bc8bd5f5a02c093e4aa3b6eda19ad2
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-24 07:55:36 +0800

    完善彩票21选5的程序

--- C语言函数小功能集锦/彩票21选5/彩票21选5/main.c
+++ C语言函数小功能集锦/彩票21选5/彩票21选5/main.c
@@ -7,9 +7,30 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    srandom((unsigned) time(0));//无符号的数，默认使long型
+    int tickets[5];
+    int i,j,k;
+    for (j=0; j<5; j++) {
+        for (i=0; i<5; i++) {
+            tickets[i]=random()%21+1;//产生1～21的随机数
+            for (k=0; k<i; k++) {//和前面的比较，万一重复了，就break
+                if (tickets[i]==tickets[k]) {
+                    break;
+                }
+            }
+            if (k==i) {
+                printf("%02d ",tickets[i]);
+            }
+            else{
+                i--;//如果有重复，本次循环作废
+            }
+        }
+        printf("\n");
+    }
     return 0;
 }
