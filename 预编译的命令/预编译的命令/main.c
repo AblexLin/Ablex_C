516bb028c02a0ef0862459467606aa0f8bdc32c7
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-23 08:31:13 +0800

    添加预编译工程文件

---
+++ 预编译的命令/预编译的命令/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  预编译的命令
+//
+//  Created by 顾明轩 on 16/4/23.
+//  Copyright © 2016年 顾明轩. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
