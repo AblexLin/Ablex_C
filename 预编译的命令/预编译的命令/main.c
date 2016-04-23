a170e88c3d495bfe05db7bafdfae260f4b6c0d9e
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-23 09:37:35 +0800

    添加记录学生成绩和老师工资的工程文件

--- 预编译的命令/预编译的命令/main.c
+++ 预编译的命令/预编译的命令/main.c
@@ -7,9 +7,23 @@
 //
 
 #include <stdio.h>
-
+#define paixu 20
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int num[paixu];
+    for (int i=0; i<paixu; i++) {
+        scanf("%d",&num[i]);
+    }
+    for (int i=0; i<paixu-1; i++) {
+        for (int j=0; j<paixu; j++) {
+            if (num[i]>num[j]) {
+                int t=num[i];
+                num[i]=num[j];
+                num[j]=t;
+            }
+        }
+    }
+    for (int x=0; x<paixu; x++) {
+        printf("%d",num[x]);
+    }
     return 0;
 }
