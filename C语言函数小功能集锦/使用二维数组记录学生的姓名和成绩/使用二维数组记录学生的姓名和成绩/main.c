137e160d05020bd1465222245a5d24dfdceedbe9
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-23 17:41:55 +0800

    终于完成了二维数组的循环赋值～～～～～

--- C语言函数小功能集锦/使用二维数组记录学生的姓名和成绩/使用二维数组记录学生的姓名和成绩/main.c
+++ C语言函数小功能集锦/使用二维数组记录学生的姓名和成绩/使用二维数组记录学生的姓名和成绩/main.c
@@ -8,27 +8,27 @@
 
 #include <stdio.h>
 #include <string.h>//包含字符串的函数
+#define STD_COUNT 3
 //包含后可以使用:strlen,strcmp,strcat,strcpy
 int main(int argc, const char * argv[]) {
     
-    char *name[100];        //先定义一个二维数组存放姓名
-    int score[100]={0};     //先定义一个一维数组存放成绩
+    char name[STD_COUNT][20]={};  //先定义一个二维数组存放姓名
+    int score[100]={};            //先定义一个一维数组存放成绩
     
-    char std_name[20];      //再定义一个一维数组存放输入的字符串
-    int std_score;          //再定义一个变量存放输入的分数
+    char std_name[20]={};         //再定义一个一维数组存放输入的字符串
+    int std_score;                //再定义一个变量存放输入的分数
     
     char *end_flag_name="end";    //定义输入姓名时结束的标志
     int end_flag_score=-1;        //定义输入分数时结束的标志
     
     printf("开始录入学生成绩...\n");
-    for (int i=0; i<2; i++) {
-
+    for (int i=0; i<STD_COUNT; i++) {
         printf("请输入学生姓名:");
         scanf("%s",std_name);       //用一维数组接受输入的字符串
         if (strcmp(std_name, end_flag_name)==0) {
             break;
         }
-        name[i]=std_name;           //将字符串赋值给二维数组
+        strcpy(*(name+i), std_name);//将字符串赋值给二维数组
     
         printf("请输入学生成绩:");
         scanf("%d",&std_score);     //用变量来接受输入的分数
@@ -36,31 +36,13 @@ int main(int argc, const char * argv[]) {
             break;
         }
         score[i]=std_score;         //将分数赋值给一维数组
-        printf("学生姓名内:%s\t学生成绩内:%d\n",name[0],score[0]);
-        printf("学生姓名内:%s\t学生成绩内:%d\n",name[1],score[1]);
     }
-//    for (int j=0; j<2; j++) {
-//    printf("学生姓名:%s\t学生成绩:%d\n",name[0],score[0]);
-//    printf("学生姓名:%s\t学生成绩:%d\n",name[1],score[1]);
-//    }
+    for (int i=0; i<STD_COUNT; i++) {
+        printf("学生姓名:%s\t学生成绩:%d\n",name[i],score[i]);
+    }
     
 //    input_child_name_score(name,score);
     
 //    output_child_name_score(name);
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
