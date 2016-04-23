4e82f1fa8002763c29abc507fc76008038a7033d
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-23 18:46:26 +0800

    优化字符串测试文件的注释

--- C语言之字符串/尝试字符串数组的循环赋值/尝试字符串数组的循环赋值/main.c
+++ C语言之字符串/尝试字符串数组的循环赋值/尝试字符串数组的循环赋值/main.c
@@ -20,6 +20,7 @@ int main(int argc, const char * argv[]) {
         printf("请输入:");
         scanf("%s",ha);//这是什么？当人是吧键盘输入的字符串给保存到ha这个数组的首地址啦～～～
         strcpy(name[i], ha);//循环给数组增加内容不就好了么？哈哈哈
+//        name[i]=ha;这样的赋值方式只适合一维数组，而name实际是二维数组，只能使用strcpy
     }
     for (int i=0; i<3; i++) {
         printf("%s\n",name[i]);
