dd164c459ad68641848ea3d06f5c97fc6bd03afc
Ablex.Gu <P4_gumingxuan@163.com>
2016-04-24 13:46:50 +0800

    添加记录学生成绩的小功能工程文件

---
+++ C语言函数小功能集锦/学生成绩记录系统/学生成绩记录系统/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  学生成绩记录系统
+//
+//  Created by 顾明轩 on 16/4/24.
+//  Copyright © 2016年 顾明轩. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
